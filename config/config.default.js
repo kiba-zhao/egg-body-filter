676095fdbf5c578d03996fdee212ccd93d17a8b8
kiba.x.zhao <kiba.rain@qq.com>
2020-11-19 15:49:10 +0800

    修改filter header名

--- config/config.default.js
+++ config/config.default.js
@@ -11,7 +11,7 @@ module.exports = app => {
   const exports = {};
 
   exports.bodyFilter = {
-    header: 'AA-Filter'.toLowerCase(),
+    header: 'Body-Filter'.toLowerCase(),
     json: ['application/json']
   };
 
